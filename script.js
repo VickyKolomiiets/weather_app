c4593461c8901793840b20a370fbc8b4516d293b
VickyKolomiiets <viktoriakolomiets24@gmail.com>
2023-05-04 18:40:46 +0300

    Updated styles

--- script.js
+++ script.js
@@ -92,7 +92,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "866a208a73eeff02182218e9441647a1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
